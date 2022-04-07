9e0e24297e19eb7120f5a3e4d6bb8224efd6a14d
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2022-04-07 11:43:21 +0700

    created testimoni section

--- vendor/js/main.js
+++ vendor/js/main.js
@@ -3,3 +3,24 @@ $(document).ready(() => {
         $('.navbar').toggleClass('scrolled', $(this).scrollTop() > $('.navbar').height())
     })
 })
+
+
+      var swiper = new Swiper(".mySwiperTesti", {
+        spaceBetween: 20,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          dynamicBullets: true,
+        },
+        autoplay: {
+            delay : 1200
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 1,
+            },
+            1200:{
+                slidesPerView: 3,
+            }
+        }
+      });
